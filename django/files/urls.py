2ce1c4b1b3d1336b966c29b8ba7278886bc6be01
zlaja-billund <mail@zlatan.dk>
2025-02-15 11:53:47 +0100

    removed slash from url

--- django/files/urls.py
+++ django/files/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import FileUploadView
 
 urlpatterns = [
-    path('/upload', FileUploadView.as_view(), name='file-upload'),
+    path('upload', FileUploadView.as_view(), name='file-upload'),
 ]
