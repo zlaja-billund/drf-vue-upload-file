cb6c97cd881dd2f01f60a4ec9fe8e5f37b6c811f
zlaja-billund <mail@zlatan.dk>
2025-02-17 13:22:56 +0100

    implemented drag and drop file

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,47 +1,113 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+import { reactive, watch } from 'vue';
+import { useDropzone } from 'vue3-dropzone';
+
+const state = reactive({
+  files: [],
+});
+
+const { getRootProps, getInputProps, isDragActive, ...rest } = useDropzone({
+  onDrop,
+});
+
+watch(state, () => {
+  console.log('state', state);
+});
+
+watch(isDragActive, () => {
+  console.log('isDragActive', isDragActive.value, rest);
+});
+
+function onDrop(acceptFiles, rejectReasons) {
+  console.log(acceptFiles);
+  console.log(rejectReasons);
+  state.files = acceptFiles;
+}
+
+function handleClickDeleteFile(index) {
+  state.files.splice(index, 1);
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+<div>
+  <div v-if="state.files.length > 0" class="files">
+    <div class="file-item" v-for="(file, index) in state.files" :key="index">
+      <span>{{ file.name }}</span>
+      <span class="delete-file" @click="handleClickDeleteFile(index)"
+        >Delete</span
+      >
     </div>
-  </header>
+  </div>
+  <div v-else class="dropzone" v-bind="getRootProps()">
+    <div
+      class="border"
+      :class="{
+        isDragActive,
+      }"
+    >
+      <input v-bind="getInputProps()" />
+      <p v-if="isDragActive">Drop the files here ...</p>
+      <p v-else>Drag and drop files here, or Click to select files</p>
+    </div>
+  </div>
+</div>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
-header {
+dropzone,
+.files {
+  width: 100%;
+  max-width: 300px;
+  margin: 0 auto;
+  padding: 10px;
+  border-radius: 8px;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+    rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+  font-size: 12px;
   line-height: 1.5;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.border {
+  border: 2px dashed #ccc;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  transition: all 0.3s ease;
+  background: #fff;
+
+  &.isDragActive {
+    border: 2px dashed #ffb300;
+    background: rgb(255 167 18 / 20%);
+  }
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+.file-item {
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: rgb(255 167 18 / 20%);
+  padding: 7px;
+  padding-left: 15px;
+  margin-top: 10px;
+
+  &:first-child {
+    margin-top: 0;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .delete-file {
+    background: red;
+    color: #fff;
+    padding: 5px 10px;
+    border-radius: 8px;
+    cursor: pointer;
   }
 }
 </style>
--- vue/src/main.js
+++ vue/src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+
 const app = createApp(App)
 
 app.use(createPinia())
